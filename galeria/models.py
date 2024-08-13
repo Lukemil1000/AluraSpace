ac8bedd6535aad6a58ae3163778fba4ab9bf4fb6
Lucas Pomar <lucaspomar@outlook.com>
2024-08-13 16:45:31 -0300

    fotografia model

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+
+    nome = models.CharField(null=False, blank=False, max_length=100)
+    legenda = models.CharField(null=False, blank=False, max_length=150)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(null=False, blank=False, max_length=100)
+
+    def __str__(self) -> str:
+        return self.nome
