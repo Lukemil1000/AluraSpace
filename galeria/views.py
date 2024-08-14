6f23058144226f6f213075cd4d047302da18f991
Lucas Pomar <lucaspomar@outlook.com>
2024-08-14 14:29:30 -0300

    index cards

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from galeria.models import Fotografia
 
 def index(request):
-        return render(request, 'galeria/index.html')
+        fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
+        return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
         return render(request, 'galeria/imagem.html')
