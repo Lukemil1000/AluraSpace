59b1fd8eeb5987ec8b4374411a7245b8af457ed5
Lucas Pomar <lucaspomar@outlook.com>
2024-08-19 16:07:32 -0300

    search bar

--- galeria/views.py
+++ galeria/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-        fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
-        return render(request, 'galeria/index.html', {"cards": fotografias})
+    fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
+
+    if "buscar" in request.GET:
+        nome = request.GET["buscar"]
+        fotografias = fotografias.filter(nome__icontains=nome)
+
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
-        fotografia = get_object_or_404(Fotografia, pk=foto_id)
-        return render(request, 'galeria/imagem.html', {"foto": fotografia})
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"foto": fotografia})
