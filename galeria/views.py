56f1ed4c3c5cedbd4f1d71c5e6e62e073d0797d6
Lucas Pomar <lucaspomar@outlook.com>
2024-09-05 15:43:32 -0300

    filtros por categoria

--- galeria/views.py
+++ galeria/views.py
@@ -13,3 +13,8 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"foto": fotografia})
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+
+    return render(request, 'galeria/index.html', {"cards": fotografias})
